2009769f48f9dbab2ec437ecf9f8c1098121eaef
Naadir Mohamed <smartnaadir@gmail.com>
2023-03-02 13:31:42 +0400

    Updated with search and PUT

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ app.use((req,res,next)=>{
 
 // logger middleware which outputs all requests to the server
 let logger = (req,res,next) => {
-    let method = req.method;
-    let url = req.url;
-    let status = res.statusCode;
-    let log = `${method}:${url} ${status}`;
-    console.log(log);
-    next();
-  };
-  
+      let method = req.method;
+      let url = req.url;
+      let status = res.statusCode;
+      let log = `${method}:${url} ${status}`;
+      console.log(log);
+      next();
+    };
+    
 app.use(logger);
 
-//Connect to the MongoDB
+// Connect to the MongoDB
 let db;
 MongoClient.connect('mongodb+srv://naadir:naadir123@cluster0.guegops.mongodb.net', (err, client) => {    
     db = client.db('app')
@@ -54,44 +54,68 @@ app.get('/collection/:collectionName',(req,res,next)=>{
     console.log("Getting the Data from Lesson Collection")
 })
 
+// Search Function
+app.get('/collection/:collectionName/:search', (req, res) => {
+    const { search } = req.params;
+    req.collection.find({}).toArray((err, results) => {
+      if (err) return next(err);
+      const lessons = results.filter(lesson => {
+        return (
+          lesson.subject.toLowerCase().match(search.toLowerCase()) ||
+          lesson.location.toLowerCase().match(search.toLowerCase())
+        );
+      });
+      res.send(lessons);
+    });
+  });
+  
+// Retrive Images
+app.use('/collection/:collectionName', (req, res, next) => {
+    var filePath = path.join(__dirname, "static/images" , req.url);
+    fs.stat(filePath, function(err, fileInfo){
+        if(err){
+            res.status(404);
+            res.send("Image file not found!");
+            return;
+        }
+        if(fileInfo.isFile()){
+            res.sendFile(filePath);
+            console.log("GET/" + req.url)
+        }
+        else next();
+    });
+});
+
 // Add the Items to Orders Collection
 app.post('/collection/:collectionName', (req, res, next) => {  
-    req.collection.insertOne(req.body, (e, results) =>
+    req.collection.insert(req.body, (e, results) =>
     {
         if (e) return next(e)    
-        res.send(results ? {msg:'success'} : {msg:'error'})
         res.send(results.ops)
     })
 })
+    
+// Update spaces for a particular lesson
+app.put('/collection/:collectionName', (req, res, next) => {
+    req.body.forEach(lesson => {
+      const lessonID = { _id: new ObjectID(lesson._id) };
+      const updateSpaces = { $set: { spaces: lesson.spaces } };
+      req.collection.updateOne( lessonID, updateSpaces,
+        { safe: true, multi: false },
+        (err, result) => {
+          if (err) return next(err);
+        }
+      );
+    });
+});
 
-//Return with objectID
+// Return with objectID
 const Objectid = require('mongodb').ObjectID;
 app.get('/collection/:collectionName/:id' , (req, res, next) => {
     req.collection.findOne({_id: new ObjectID(req.params.id)} , (e,results) => {
         if (e) return next(e)
         res.send(results)
     })
-})
-
-//Update
-app.put('/collection/:collectionName/:id', (req,res,next) => {
-    req.collection.updateOne(
-        {_id: new ObjectID(req.params.id)},
-        {$set: req.body},
-        {safe:true, multi:false}, (e,results) => {
-            if (e) return next(e)
-            res.send((results = 1) ? {msg:'success'} : {msg:'error'})
-        })
-})
-
-//Delete
-app.delete('/collection/:collectionName/:id', (req,res,next) => {
-    req.collection.deleteOne(
-        {_id: ObjectID(req.params.id)},
-        (e,results) => {
-            if (e) return next(e)
-            res.send(results ? {msg:'success'} : {msg:'error'})
-        })
 })
 
 // If it is not a GET request, also servers a 404 error.
